debd1ca5a1b7f5b54fa228b5072d8691f007554d
yocn <yoc.yocn@gmail.com>
2024-06-12 16:12:47 +0900

    finish react foundations

---
+++ react_foundations/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O6Oue5HnCRG0Zg5dd2ifvkD1iJz6dJdEaQ16LYjhbRA=\"\n}"
---
+++ react_foundations/app/page.js
@@ -0,0 +1,21 @@
+import LikeButton from './like-button';
+
+function Header(props) {
+  return <h1>{props.title}</h1>;
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  return (
+    <div>
+      <Header title='Develop. Preview. Ship.' />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </div>
+  );
+}
